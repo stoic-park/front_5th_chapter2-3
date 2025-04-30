59b738d2b1c55f957b624367443fd214dad16c0f
stoic-park <demige79@gmail.com>
2025-04-30 09:12:38 +0900

    feat: entities/user/ui UserAvatar분리

---
+++ src/entities/user/ui/UserAvatar.tsx
@@ -0,0 +1,18 @@
+// src/entities/user/ui/UserAvatar.tsx
+
+import { User } from "../model/types"
+
+interface UserAvatarProps {
+  user: User
+  onClick?: (user: User) => void
+}
+
+// 순수하게 props를 받아서 user에 대한 ui 를 담당하는 부분
+export const UserAvatar = ({ user, onClick }: UserAvatarProps) => {
+  return (
+    <div className="flex items-center gap-2 cursor-pointer" onClick={() => onClick?.(user)}>
+      <img src={user.image} alt={user.username} className="w-8 h-8 rounded-full object-cover" />
+      <span className="truncate">{user.username}</span>
+    </div>
+  )
+}
